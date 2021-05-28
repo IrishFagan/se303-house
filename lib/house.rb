651915681285b95c227e0cb50a8eaa4a085c647b
Irish Fagan <IrishFagan@live.com>
2021-05-27 17:28:12 -0700

    Revise line method to use phrases method instead of verse

--- lib/house.rb
+++ lib/house.rb
@@ -1,8 +1,12 @@
 class House
+	def beginning
+		"This is"
+	end
+
 	def phrases(number)
 		phrases = [
 			"the house that Jack built.\n",
-			"the malt that lay in the house",
+			"the malt that lay in",
 			"the rat that ate",
 			"the cat that killed",
 			"the dog that worried",
@@ -14,29 +18,11 @@ class House
 			"the farmer sowing his corn that kept",
 			"the horse and the hound and the horn that belonged to"
 		]
-		phrases(number - 1)
-	end
-
-	def verse(number)
-		verse = [
-		 "This is the house that Jack built.\n",
-		 "This is the malt that lay in the house that Jack built.\n",
-		 "This is the rat that ate the malt that lay in the house that Jack built.\n",
-		 "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-		 "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-		 "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-		 "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-		 "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-  	 "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-  	 "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-  	 "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-  	 "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-		]
-		verse[number - 1]
+		phrases[number - 1]
 	end
 
 	def line(number)
-		verse(number)
+		"#{beginning} #{number.downto(1).collect { |i| phrases(i) }.join(" ")}"
 	end
 
 	def recite
