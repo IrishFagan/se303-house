3a9494110fe8878493fc06e190d53d737665132a
Irish Fagan <IrishFagan@live.com>
2021-05-27 17:14:19 -0700

    Add phrases method

--- lib/house.rb
+++ lib/house.rb
@@ -1,4 +1,22 @@
 class House
+	def phrases(number)
+		phrases = [
+			"the house that Jack built.\n",
+			"the malt that lay in the house",
+			"the rat that ate",
+			"the cat that killed",
+			"the dog that worried",
+			"the cow with the crumpled horn that tossed",
+			"the maiden all forlorn that milked",
+			"the man all tattered and torn that kissed",
+			"the priest all shaven and shorn that married",
+			"the rooster that crowed in the morn that woke",
+			"the farmer sowing his corn that kept",
+			"the horse and the hound and the horn that belonged to"
+		]
+		phrases(number - 1)
+	end
+
 	def verse(number)
 		verse = [
 		 "This is the house that Jack built.\n",
